5655213c4ddcfeba67f4c1a61da077a8a824601f
Yuri Nunes Atzler <atzleryuri@gmail.com>
2023-01-16 10:10:05 -0300

    add: favicon e projetos

--- src/assets/projects/index.js
+++ src/assets/projects/index.js
@@ -1,5 +1,7 @@
 import pageZokaSass from "./page-zoka-sass.png";
 import linkedin from "./linkedin.png";
+import cloneTrello from "./clone-trello.png";
+import remindersApp from "./reminders-app.png";
 
 export const ArrProjects = [
   {
@@ -19,67 +21,19 @@ export const ArrProjects = [
     link: "https://homework-9.vercel.app/",
   },
   {
-    name: "Zoka com Sass",
-    img: pageZokaSass,
+    name: "Clone Trello",
+    img: cloneTrello,
     description:
-      "Projeto realizado com Sass, html e css puro. A idéia era praticar o uso do Sass.",
-    date: "Oct 14, 2022",
-    link: "https://page-zoka-sass.vercel.app/",
+      "Projeto focado em práticar html e css, a idéia era criar um clone simples do Trello sem funcionalidades.",
+    date: "Oct 15, 2022",
+    link: "https://clone-trello-nine.vercel.app/",
   },
   {
-    name: "Linkedin",
-    img: linkedin,
+    name: "Reminders App",
+    img: remindersApp,
     description:
-      "Projeto realizado em dupla práticando html e css, a idéia era por em prática um novo conceito criado para o linkedin.",
-    date: "Oct 17, 2022",
-    link: "https://homework-9.vercel.app/",
-  },
-  {
-    name: "Zoka com Sass",
-    img: pageZokaSass,
-    description:
-      "Projeto realizado com Sass, html e css puro. A idéia era praticar o uso do Sass.",
-    date: "Oct 14, 2022",
-    link: "https://page-zoka-sass.vercel.app/",
-  },
-  {
-    name: "Linkedin",
-    img: linkedin,
-    description:
-      "Projeto realizado em dupla práticando html e css, a idéia era por em prática um novo conceito criado para o linkedin.",
-    date: "Oct 17, 2022",
-    link: "https://homework-9.vercel.app/",
-  },
-  {
-    name: "Zoka com Sass",
-    img: pageZokaSass,
-    description:
-      "Projeto realizado com Sass, html e css puro. A idéia era praticar o uso do Sass.",
-    date: "Oct 14, 2022",
-    link: "https://page-zoka-sass.vercel.app/",
-  },
-  {
-    name: "Linkedin",
-    img: linkedin,
-    description:
-      "Projeto realizado em dupla práticando html e css, a idéia era por em prática um novo conceito criado para o linkedin.",
-    date: "Oct 17, 2022",
-    link: "https://homework-9.vercel.app/",
-  },
-  {
-    name: "Zoka com Sass",
-    img: pageZokaSass,
-    description:
-      "Projeto realizado com Sass, html e css puro. A idéia era praticar o uso do Sass.",
-    date: "Oct 14, 2022",
-    link: "https://page-zoka-sass.vercel.app/",
-  },
-  {
-    name: "Linkedin",
-    img: linkedin,
-    description:
-      "Projeto realizado em dupla práticando html e css, a idéia era por em prática um novo conceito criado para o linkedin.",
-    date: "Oct 17, 2022",
-    link: "https://homework-9.vercel.app/",
+      "Um simples app para salvar tasks ao longo do tempo, práticar a manipulação de datas.",
+    date: "Oct 22, 2022",
+    link: "https://homework-5-js.vercel.app/",
   },
 ];
